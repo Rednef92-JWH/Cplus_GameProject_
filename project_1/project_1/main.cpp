918d7a927f649a747d406938bfd85e0697750137
Rednef92-JWH <rednef92@gmail.com>
2020-03-03 00:16:32 -0800

    Cplus_GameProject_settings
    Comments.

---
+++ project_1/project_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project_1
+//
+//  Created by Root_ on 2/27/20.
+//  Copyright © 2020 Root_. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
